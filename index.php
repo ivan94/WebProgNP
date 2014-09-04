5e0c9573e51256c21cfb79e016457b5f1ae9f617
unknown <fernandi2@LC112PC13.active.newpaltz.edu>
2014-09-04 10:31:09 -0400

    Index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>HTML</title>
+	</head>
+	
+	<body>
+		<div>
+			<h1>Edited on Aptana</h1>
+			<h2>Today is Sep 4th</h2>
+		</div>
+	</body>
+</html>
