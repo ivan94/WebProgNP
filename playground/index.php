94c5f7f436e7a59ad5bb7360d64d5d08300f78c8
unknown <fernandi2@LC112PC11.active.newpaltz.edu>
2014-09-08 10:46:22 -0400

    Bootstrap and jquery

--- playground/index.php
+++ playground/index.php
@@ -3,12 +3,40 @@
 <html lang="en">
 	<head>
 		<title>HTML</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+		<style type="text/css">
+		</style>
+		
 	</head>
 	
 	<body>
 		<div>
-			<h1>Edited on Aptana</h1>
-			<h2>Today is Sep 4th</h2>
+			<header>
+				<div class="container">
+					<h1>Edited on Aptana</h1>
+					<h2>Today is Sep 4th</h2>
+				</div>
+			</header>
+			<nav>
+				<div class="container">
+					<p>
+						<a href="/">Home</a>
+					</p>
+				</div>
+			</nav>
+			
+			<footer>
+				<div class="container">
+					<p>
+						&copy; Copyright by Ivan
+					</p>
+				</div>
+			</footer>
 		</div>
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
